876e040422058feb85656f7e5141622938edb312
AgaevAlex <agaevalex97@gmail.com>
2019-03-20 00:23:12 +0300

    Добавление JavaDoc для класса, метода и package

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,14 @@
 package ru.job4j.calculate;
-
+/** 
+ * Class Calculate для вывода "Hello World!" 
+ * @author Alex Agaev (agaevalex97@gmail.com)
+ * @since 20.03.2019
+*/
 public class Calculate {
-	
+	/** 
+	* Конструктор, вывод строки в консоль.
+	* @param arg - arg 
+	*/
 	public static void main(String[]args) {
 		System.out.println("Hello World!");
 	}
