73c140cc6910c7d727e67543139786478eff43d2
Alexey <agaevalex97@gmail.com>
2020-03-29 16:20:13 +0300

    2. Написать программу преобразования List в Map.[#122544]

---
+++ collection/src/main/java/ru/job4j/collection/OrderConvert.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+        import java.util.HashMap;
+        import java.util.List;
+        import java.util.Map;
+
+public class OrderConvert {
+    public static HashMap<String, Order> process(List<Order> orders) {
+        HashMap<String, Order> map = new HashMap<>();
+
+        for (Order orders1 : orders){
+            map.put(orders1.getNumber(),orders1);
+            System.out.println(orders1);
+        }
+        return map;
+    }
+}
