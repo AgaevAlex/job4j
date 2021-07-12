bef553cbf1f79701de2503cc747b8209af8a4066
AgaevAlex <agaevalex97@gmail.com>
2021-07-13 00:16:16 +0300

    1. Паспорт и жители [#212656 #176636]

--- chapter_003/src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -14,4 +14,11 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
     }
+    @Test
+    public void addDuplicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.add(citizen),is(false));
+    }
 }
